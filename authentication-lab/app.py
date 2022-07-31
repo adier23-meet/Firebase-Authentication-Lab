57b6ba708ed2e46f759b38dedd03546ee3ea95f4
adier23-meet <109903931+adier23-meet@users.noreply.github.com>
2022-07-31 14:12:15 +0300

    keith

--- authentication-lab/app.py
+++ authentication-lab/app.py
@@ -2,18 +2,54 @@ from flask import Flask, render_template, request, redirect, url_for, flash
 from flask import session as login_session
 import pyrebase
 
+
+config = {
+  "apiKey": "AIzaSyBTkRd6sHfMXPBZW0qgW1l6eCmBkqXNSo4",
+  "authDomain": "csss-17e98.firebaseapp.com",
+  "projectId": "csss-17e98",
+  "storageBucket": "csss-17e98.appspot.com",
+  "messagingSenderId": "304723448287",
+  "appId": "1:304723448287:web:b8e8381ce105f4f15ff46a",
+  "measurementId": "G-NWP85NS83X",
+  "databaseURL": "https://csss-17e98-default-rtdb.europe-west1.firebasedatabase.app/"
+}
+
+firebase = pyrebase.initialize_app(config)
+auth = firebase.auth()
+db = firebase.database()
+
+
 app = Flask(__name__, template_folder='templates', static_folder='static')
 app.config['SECRET_KEY'] = 'super-secret-key'
 
 
 @app.route('/', methods=['GET', 'POST'])
 def signin():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        try:
+            login_session['user'] = auth.sign_in_with_email_and_password(email, password)
+            return redirect(url_for('home'))
+        except:
+            error = "Authentication failed"
     return render_template("signin.html")
+
 
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
-    return render_template("signup.html")
+    if request.method == 'POST':
+       email = request.form['email']
+       password = request.form['password']
+       try:
+            login_session['user'] = auth.create_user_with_email_and_password(email, password)
+            return redirect(url_for('home'))        
+       except:
+            error = "Authentication failed"
+            return render_template("signup.html")
+
+
 
 
 @app.route('/add_tweet', methods=['GET', 'POST'])
@@ -21,5 +57,13 @@ def add_tweet():
     return render_template("add_tweet.html")
 
 
+@app.route('/signout')
+def signout():
+    login_session['user'] = None
+    auth.current_user = None
+    return redirect(url_for('signin'))
+
+
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port = 5001)
